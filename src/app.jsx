09b06ea32554e88181c532b646820ecf68a53b7a
Tobira <tobirawork@gmail.com>
2017-06-24 16:20:08 +0300

    set prod config, nex step - add postcss plugins

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+import './App.styl'
+
 class App extends React.Component {
     render() {
         return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackDashBoard = require('webpack-dashboard');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = env => {
     if (env === 'production') {
         return PROD;
-    } 
+    }
     if (env === 'development') {
         return DEV;
     }
@@ -35,16 +35,17 @@ const DEV = {
                 test: /\.styl$/,
                 use: [
                     'style-loader',
-                    { 
-                        loader: 'css-loader', 
+                    {
+                        loader: 'css-loader',
                         options: {
                             modules: true,
-                            localIdentName: '[local]--[hash:base64:5]--[name]'
-                        } 
+                            localIdentName: '[name]--[hash:base64:5]'
+                        }
                     },
+                    //'postcss-loader',
                     'stylus-loader'
                 ]
-            },   
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
@@ -76,5 +77,61 @@ const DEV = {
         }),
 
         new webpack.HotModuleReplacementPlugin()
+    ]
+};
+
+const PROD = {
+    entry: './src/App.jsx',
+
+    output: {
+        filename: 'application.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                use: [
+                    'babel-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.styl$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        //'postcss-loader',
+                        'stylus-loader',
+                    ],
+                }),
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+
+    devtool: 'sourse-map',
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '*']
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/app.html'
+        }),
+
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('application.css')
     ]
 }
